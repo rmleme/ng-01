1062f0788af008486c81a6474d38cd7ea0978066
Rodrigo Leme <rmleme@gmail.com>
2019-06-10 21:31:54 +0000

    Build app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
