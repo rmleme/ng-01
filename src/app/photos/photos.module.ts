7dea45cf8460c7efb65b879e6d7f54e03b2a4c43
Rodrigo Leme <rmleme@gmail.com>
2019-06-24 20:43:01 +0000

    Routing.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,19 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from "@angular/common";
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    declarations: [
+        PhotoComponent,
+        PhotoListComponent,
+        PhotoFormComponent
+    ],
+    imports: [
+        HttpClientModule,
+        CommonModule
+    ]
 })
 export class PhotosModule {}
