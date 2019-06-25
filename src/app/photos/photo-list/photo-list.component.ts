4d22490d9971b0753a40b0fccc4ab7a8f6e53859
Rodrigo Leme <rmleme@gmail.com>
2019-06-25 17:49:36 +0000

    New component.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,7 +4,7 @@ import { Photo } from '../photo/photo';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
