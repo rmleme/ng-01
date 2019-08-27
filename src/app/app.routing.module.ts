de0a90ccc240f7b3357198cccc4ce09fcf26b1e5
Rodrigo Leme <rmleme@gmail.com>
2019-08-27 20:19:22 +0000

    routerLink.

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,11 +5,13 @@ import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 import { SignInComponent } from "./home/signin/signin.component";
+import { AuthGuard } from "./core/auth/auth.guard";
 
 const routes: Routes = [
     {
         path: '',
-        component: SignInComponent
+        component: SignInComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'user/:userName',
@@ -29,8 +31,8 @@ const routes: Routes = [
 ]
 
 @NgModule({
-    imports: [ RouterModule.forRoot(routes) ],
-    exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
