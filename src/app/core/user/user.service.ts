f07fa5af79f525187144320ed2f45d4e98ac9484
Rodrigo Leme <rmleme@gmail.com>
2019-08-27 18:54:45 +0000

    Improving front-end header; logout.

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { BehaviorSubject } from 'rxjs';
+import { User } from './user';
+import * as jwt_decode from 'jwt-decode';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+
+    private userSubject = new BehaviorSubject<User>(null);
+
+    constructor(private tokenService: TokenService) {
+        if (this.tokenService.hasToken()) {
+            this.decodeAndNotify();
+        }
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+        this.userSubject.next(user);
+    }
+
+    logout() {
+        this.tokenService.removeToken();
+        this.userSubject.next(null);
+    }
+}
