8aa596b3739729882c0cc2df3f9daa267fee5479
Rodrigo Leme <rmleme@gmail.com>
2019-09-26 23:17:53 +0000

    Comments feature.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from "@angular/common";
 import { PhotoModule } from "./photo/photo.module";
 import { PhotoFormModule } from "./photo-form/photo-form.module";
 import { PhotoListModule } from "./photo-list/photo-list.module";
+import { PhotoDetailsModule } from "./photo-details/photo-details.module";
 
 @NgModule({
     imports: [
         PhotoModule,
         PhotoFormModule,
         PhotoListModule,
+        PhotoDetailsModule,
         HttpClientModule,
         CommonModule
     ]
